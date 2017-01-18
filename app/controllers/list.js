fead6825771c38194585895324dc969892325c09
Sorrel Brigman <sorrel.k@gmail.com>
2017-01-18 11:01:58 -0600

    pause, mid writing for in loop

--- app/controllers/list.js
+++ app/controllers/list.js
@@ -1,4 +1,21 @@
-app.controller("ListCtrl", function($scope) {
-  $scope.userName = ""
-  $scope.notes = ""
+app.controller("ListCtrl", function($scope, $http) {
+
+  getCurrentUser();
+
+  $http.get(`https://classprojectusernotes.firebaseio.com/${currentUser.uid}/notes.json`)
+  .then((val) => {
+    let myNotes = []
+    for(var key in val) {
+      if (val.data[key] !== undefined ) {
+        myNotes.push(val.data[key])
+        console.log("val.data[key]", val.data[key])
+      }
+    }
+
+    console.log("val: ", val)
+    console.log("myNotes: ", myNotes)
+    $scope.notes = myNotes;
+
+  })
+
 })
