e67007d63deff8e435793fa8e7ef1335d515e8a3
Sorrel Brigman <sorrel.k@gmail.com>
2017-01-18 10:23:12 -0600

    can add user notes to specific user account

--- app/controllers/newnote.js
+++ app/controllers/newnote.js
@@ -1,8 +1,10 @@
 app.controller("NewNoteCtrl", function($scope, $http) {
   $scope.saveNote = function() {
     let newNote = $scope.newNote;
+    let currentUrl = `https://classprojectusernotes.firebaseio.com/${currentUser.uid}/notes.json`;
+    console.log("current URL", currentUrl)
     console.log($scope.newNote)
-    $http.post(`https://classprojectusernotes.firebaseio.com/${currentUser.uid}/notes.json`, newNote)
+    $http.post(currentUrl, JSON.stringify(newNote))
     .then((e)=>{
       console.log("success, new note saved", e)
     })
--- app/main.js
+++ app/main.js
@@ -37,6 +37,16 @@ function getCurrentUser() {
   currentUser = firebase.auth().currentUser;
 }
 
+function showLogInPage() {
+  if (currentUser !== null) {
+//show rest of site
+  } else {
+//keep them locked out on the login/reg page
+  }
+}
+
+
+firebase.auth().onAuthStateChanged(getCurrentUser);
 firebase.auth().onAuthStateChanged(getCurrentUser);
 
 
@@ -46,92 +56,3 @@ firebase.auth().onAuthStateChanged(getCurrentUser);
 
 
 const app = angular.module("noteApp", ["ngRoute"]);
-
-app.config(($routeProvider, $locationProvider) => {
-  $locationProvider.hashPrefix("");
-
-  $routeProvider
-  .when("/register", {
-    controller : "RegCtrl",
-    templateUrl : "partials/reg.html"
-  })
-  .when("/login", {
-    controller : "LoginCtrl",
-    templateUrl : "partials/login.html"
-  })
-  .when("/notes", {
-    controller : "ListCtrl",
-    templateUrl : "partials/list.html"
-  })
-  .when("/new", {
-    controller : "NewNoteCtrl",
-    templateUrl : "partials/newnote.html"
-  })
-  .otherwise({
-    redirectTo: "/register"
-  })
-});
-
-
-app.controller("RegCtrl", function($scope, $http) {
-
-
-  //get user name
-  $scope.registerUser = function() {
-    let email = $("input[type='email']").val();
-    let password = $("input[type='password']").val();
-    let userName = $("input[type='text']").val();
-
-    //
-
-      firebase
-      .auth()
-      .createUserWithEmailAndPassword(email, password)
-      .then((e)=>{
-        console.log(e)})
-      .catch((e)=>{
-        alert(e)
-      })
-    }
-
-})
-
-
-app.controller("LoginCtrl", function($scope) {
-  $scope.loginUser = function() {
-    let email = $("input[type='email']").val();
-    let password = $("input[type='password']").val();
-
-    //
-      console.log("email", email);
-
-      firebase
-      .auth()
-      .signInWithEmailAndPassword(email, password)
-      .then((e)=>{
-        console.log(e)})
-      .catch((e)=>{
-        alert(e)
-      })
-    }
-
-})
-
-app.controller("ListCtrl", function($scope) {
-  $scope.userName = ""
-  $scope.notes = ""
-})
-
-app.controller("NewNoteCtrl", function($scope, $http) {
-  $scope.saveNote = function() {
-    let newNote = $scope.newNote;
-    console.log($scope.newNote)
-    $http.post(`https://classprojectusernotes.firebaseio.com/${currentUser.uid}/notes.json`, newNote)
-    .then((e)=>{
-      console.log("success, new note saved", e)
-    })
-    .catch((e)=>{
-      alert(e)
-    })
-  }
-})
